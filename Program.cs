e142d968158da379f6d68946b96b60952fce4869
Victor Pineda <pinedavictor095@gmail.com>
2019-07-22 22:29:57 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Google.Cloud.Translation.V2;
+
+namespace uisapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "file.txt";
+            StreamWriter outputFile = new StreamWriter("output.txt");
+            string line = "";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("----------PROGRAM START--------------");
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string currentLine;
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        line = Translate(currentLine).TranslatedText;
+                        outputFile.WriteLine(line);
+                    }
+                    outputFile.Close();                    
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("ERROR: FILE NOT FOUND");
+            }
+        }
+        static TranslationResult Translate(string text)
+        {
+            var client = TranslationClient.Create();
+            return client.TranslateText(text, LanguageCodes.Spanish);
+        }
+    }
+}
