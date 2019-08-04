66bb7f79b77f8e8649e2aa269e79c7c810052c4b
Victor Pineda <pinedavictor095@gmail.com>
2019-08-04 06:31:32 -0700

    updated string array to list

--- Program.cs
+++ Program.cs
@@ -8,23 +8,23 @@ namespace uisapp
     class Program
     {
         static void Main(string[] args)
-        {
-            int paragraphSize = 7;
+        {            
             string path = "file.txt";
             StreamWriter outputFile = new StreamWriter("output.txt");
-            string[] paragraph = new string[paragraphSize];
-            int count = 0;
+            List<string> paragraph = new List<string>();
             if (File.Exists(path))
             {
                 Console.WriteLine("----------PROGRAM START--------------");
                 using (StreamReader reader = new StreamReader(path))
                 {
-                    // TODO: Loop structure needs to be updated                    
-                    string currentLine;                
+                    string currentLine;
                     while ((currentLine = reader.ReadLine()) != null)
                     {
-                        paragraph[count] = currentLine;                        
-                        count++;
+                        paragraph.Add(currentLine);
+                    }
+                    IList<TranslationResult> translatedParagraph = Translate(paragraph);
+                    foreach(string s in paragraph){
+                        outputFile.WriteLine(s);
                     }
                     outputFile.Close();
                     Console.WriteLine("----------PROGRAM END--------------");
@@ -34,11 +34,11 @@ namespace uisapp
             {
                 throw new FileNotFoundException("ERROR: FILE NOT FOUND");
             }
-        }
+        }        
         static IList<TranslationResult> Translate(IEnumerable<string> text)
         {
             var client = TranslationClient.Create();
-            return client.TranslateText(text, LanguageCodes.Spanish);
+            return client.TranslateText(text, LanguageCodes.Spanish);            
         }
     }
 }
